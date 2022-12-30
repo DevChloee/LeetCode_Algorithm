22c7d64d24ce5bcc86cc5e445bf255ee544968c9
DevChloee <dawon9031@gmail.com>
2022-12-29 22:03:45 -0500

    ADD | solved question 53 in Leetcode | Maximum subarray | usign kadane algorithm

--- exercises/LC53_maxSubArray/index.js
+++ exercises/LC53_maxSubArray/index.js
@@ -1,3 +1,11 @@
-function maxSubArray(nums) {}
+function maxSubArray(nums) {
+
+    let cur=nums[0];
+    let max=nums[0];
+    for (let i = 1; i < nums.length; i++){
+        cur = Math.max(cur+nums[i],nums[i]);
+        max = Math.max(cur, max);
+    }
+}
 
 module.exports = maxSubArray;
